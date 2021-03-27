0801003a34405d1a2601054288254d8becdc3467
GuiFPires <irespi1@hotmail.com>
2021-03-27 12:17:53 +0000

    now counts classes and inner classes

--- src/main/java/es2sem2021/grupo2/codequalityassessor/metrics/extractors/NOM.java
+++ src/main/java/es2sem2021/grupo2/codequalityassessor/metrics/extractors/NOM.java
@@ -3,16 +3,20 @@ package es2sem2021.grupo2.codequalityassessor.metrics.extractors;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
+import com.github.javaparser.ParseProblemException;
 import com.github.javaparser.StaticJavaParser;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.body.BodyDeclaration;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 import com.github.javaparser.ast.body.ConstructorDeclaration;
 import com.github.javaparser.ast.body.MethodDeclaration;
 import com.github.javaparser.ast.body.TypeDeclaration;
 import com.github.javaparser.ast.visitor.VoidVisitor;
-import com.github.javaparser.ast.visitor.VoidVisitorAdapter; 
+import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
+import com.github.javaparser.utils.Pair; 
 
 public class NOM {
 	private static class MethodNameCollector extends VoidVisitorAdapter<List<String>> {			
@@ -30,7 +34,52 @@ public class NOM {
 			collector.add(md.getDeclarationAsString());
 		}
 	}
-	public static int nom(String filepath){
+	
+	public static HashMap<String,Integer> getClassNOM(File f){
+		try {
+			CompilationUnit compilationUnit = StaticJavaParser.parse(f);
+			ArrayList<Pair<String, Integer>> classes = new ArrayList<>();
+			ClassNameCollector className = new ClassNameCollector();
+			className.visit(compilationUnit, classes);
+			return getResults(classes);
+		} catch (FileNotFoundException | ParseProblemException e) {
+			return null;
+		}
+	}
+	
+	private static HashMap<String, Integer> getResults(ArrayList<Pair<String, Integer>> classes) {
+		if (classes.size()<1) return null;
+		
+		Pair<String, Integer> main = classes.remove(classes.size()-1);
+		HashMap<String, Integer> results = new HashMap<>();
+		
+		results.put(main.a, main.b);
+		for (Pair<String, Integer> pair: classes) {
+			results.put(main.a+ "." + pair.a, pair.b);
+		}
+		return results;
+	}
+	
+	public static class ClassNameCollector extends VoidVisitorAdapter<List<Pair<String, Integer>>>{
+	    @Override
+	    public void visit(ClassOrInterfaceDeclaration n, List<Pair<String, Integer>> collector) {
+	        super.visit(n, collector);
+	        int methods = countMethods(n);
+	        collector.add(new Pair<String, Integer>(n.getNameAsString(), methods));
+	    }
+	}
+	
+	public static int countMethods(ClassOrInterfaceDeclaration n) {
+		int methods = 0;
+		for(MethodDeclaration method : n.getMethods()) {
+			methods++;
+		}
+		for(ConstructorDeclaration constructor : n.getConstructors()) {
+			methods++;
+		}
+		return methods;
+	}
+	/*public static int nom(String filepath){
 		int res=0;
 		try {
 			CompilationUnit compilationUnit = StaticJavaParser.parse(new File(filepath));
@@ -63,5 +112,5 @@ public class NOM {
 		}
 		return res;
 
-	}
+	}*/
 }
--- src/test/java/es2sem2021/grupo2/codequalityassessor/metrics/extractors/NOMTest.java
+++ src/test/java/es2sem2021/grupo2/codequalityassessor/metrics/extractors/NOMTest.java
@@ -2,23 +2,48 @@ package es2sem2021.grupo2.codequalityassessor.metrics.extractors;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.File;
+import java.util.HashMap;
+
 import org.junit.jupiter.api.Test;
 
 class NOMTest {
 
 	@Test
 	void testGrammerException() {
-		assertEquals(4,NOM.nom("testFiles\\src\\com\\jasml\\compiler\\GrammerException.java"));
+		File f = new File("testFiles\\src\\com\\jasml\\compiler\\GrammerException.java");
+		HashMap<String,Integer> results=NOM.getClassNOM(f);
+		HashMap<String,Integer> expected= new HashMap<>();
+		expected.put("GrammerException", 4);
+		assertEquals(results,expected);
+		
 	}
 	
 	@Test
 	void testParsingException() {
-		assertEquals(6,NOM.nom("testFiles\\src\\com\\jasml\\compiler\\ParsingException.java"));
+		File f = new File("testFiles\\src\\com\\jasml\\compiler\\ParsingException.java");
+		HashMap<String,Integer> results=NOM.getClassNOM(f);
+		HashMap<String,Integer> expected= new HashMap<>();
+		expected.put("ParsingException", 6);
+		assertEquals(results,expected);
 	}
 	
 	@Test
 	void testSourceCodeParser() {
-		assertEquals(29,NOM.nom("testFiles\\src\\com\\jasml\\compiler\\SourceCodeParser.java"));
+		File f = new File("testFiles\\src\\com\\jasml\\compiler\\SourceCodeParser.java");
+		HashMap<String,Integer> results=NOM.getClassNOM(f);
+		HashMap<String,Integer> expected= new HashMap<>();
+		expected.put("SourceCodeParser", 29);
+		expected.put("SourceCodeParser.OpcodeWrapper", 2);
+		expected.put("SourceCodeParser.LabeledInstructions", 1);
+		assertEquals(results,expected);
+	}
+	
+	@Test
+	void testFNF() {
+		File f = new File("dasdasdas");
+		HashMap<String,Integer> results=NOM.getClassNOM(f);
+		assertNull(results);
 	}
 	
 
