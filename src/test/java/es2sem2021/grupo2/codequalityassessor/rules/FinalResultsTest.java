8c1b1fffd0c54a837d523f829986dc7994d0a4ed
tiagojoao2000 <tjbpa@iscte-iul.pt>
2021-04-30 19:14:18 +0100

    FinalResults Testing and Debugging

---
+++ src/test/java/es2sem2021/grupo2/codequalityassessor/rules/FinalResultsTest.java
@@ -0,0 +1,42 @@
+package es2sem2021.grupo2.codequalityassessor.rules;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import es2sem2021.grupo2.codequalityassessor.metrics.MetricsExtractor;
+import es2sem2021.grupo2.codequalityassessor.metrics.extractors.LOC_method;
+import es2sem2021.grupo2.codequalityassessor.xlsx.Method;
+
+public class FinalResultsTest {
+	
+	
+	@Test
+	void getRulesResultsTest() {
+		File f = new File("testFiles\\src\\com\\jasml\\compiler\\ParsingException.java");	
+		try {
+			List<Method> methods = MetricsExtractor.extract(f);
+			assertNotNull(methods);
+			RulesSet.rules.clear();
+			RulesSet.addRule("Teste", "LOC_Method >= 5");
+			ArrayList<RuleResults> ruleResults = FinalResults.getRulesResults(methods);
+			System.out.println(ruleResults.get(0).getMethodsresults().get(5).getMethod());
+			System.out.println(ruleResults.get(0).getMethodsresults().get(5).getResult());
+			assertEquals(1,ruleResults.size());
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
+	
+}
