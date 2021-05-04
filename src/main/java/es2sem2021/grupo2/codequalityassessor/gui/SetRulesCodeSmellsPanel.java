5ed49ae4f5d0b6b1b7a130bb1921c565a5b30dc2
Afonso Esteves <afonsomgesteves@gmail.com>
2021-05-04 16:54:16 +0100

    Continued implementation of code smells rules

--- src/main/java/es2sem2021/grupo2/codequalityassessor/gui/SetRulesCodeSmellsPanel.java
+++ src/main/java/es2sem2021/grupo2/codequalityassessor/gui/SetRulesCodeSmellsPanel.java
@@ -1,16 +1,23 @@
 package es2sem2021.grupo2.codequalityassessor.gui;
 
+import java.awt.event.ActionEvent;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.DefaultCellEditor;
+import javax.swing.JComboBox;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.ScrollPaneConstants;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumn;
 
+import es2sem2021.grupo2.codequalityassessor.rules.CodeSmells;
 import es2sem2021.grupo2.codequalityassessor.rules.FinalResults;
 import es2sem2021.grupo2.codequalityassessor.rules.Rule;
 import es2sem2021.grupo2.codequalityassessor.rules.RuleResults;
@@ -42,16 +49,35 @@ public class SetRulesCodeSmellsPanel extends JPanel {
 		table.setFillsViewportHeight(true);
 		scrollPane.setBounds(27, 185, 591, 275);
 		add(scrollPane);
+		
+		updateCodeSmells();
+		
 	}
 	
-	private void updateRules() {
-		HashMap<String, Rule> rules = RulesSet.getRules();
+	Action change = new AbstractAction()
+	{
+	    public void actionPerformed(ActionEvent e)
+	    {
+	    	JTable table = (JTable)e.getSource();
+	        int modelRow = Integer.valueOf( e.getActionCommand() );
+	        String s = table.getModel().getValueAt(modelRow, 1).toString();
+	    }
+	};
+	
+	private void updateCodeSmells() {
+		CodeSmells.importMandatoryCodeSmells();
+		HashMap<String, Rule> codeSmells = CodeSmells.getCodesmells();
 		model.getDataVector().removeAllElements();
 		revalidate();
-		for (Map.Entry<String, Rule> entry : rules.entrySet()) {
-			String key = entry.getKey();
-			Rule rule = entry.getValue();
-			model.addRow(new Object[] { key, rule.getConditions(), "Delete", "Change" });
+		for (Map.Entry<String, Rule> entry : codeSmells.entrySet()) {
+			JComboBox<String> comboBox = new JComboBox<>();
+			HashMap<String, Rule> rules = RulesSet.getRules();
+			for (Map.Entry<String, Rule> rule : rules.entrySet()) {
+				comboBox.addItem(rule.getKey());
+				TableColumn column = table.getColumnModel().getColumn(1);
+				column.setCellEditor(new DefaultCellEditor(comboBox));
+			}
+			model.addRow(new Object[] { entry.getKey(), entry.getValue().getConditions()});
 		}
 	}
 }
--- src/main/java/es2sem2021/grupo2/codequalityassessor/rules/CodeSmells.java
+++ src/main/java/es2sem2021/grupo2/codequalityassessor/rules/CodeSmells.java
@@ -5,6 +5,10 @@ import java.util.HashMap;
 public class CodeSmells {
 	static HashMap<String,Rule> codesmells = new HashMap<>();
 	
+	public static HashMap<String, Rule> getCodesmells() {
+		return codesmells;
+	}
+
 	public static void importMandatoryCodeSmells() {
 		codesmells.put("is_Long_Method", null);
 		codesmells.put("is_God_Class", null);
