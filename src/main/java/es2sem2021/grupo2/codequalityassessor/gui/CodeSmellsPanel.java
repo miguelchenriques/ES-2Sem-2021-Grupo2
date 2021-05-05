dddb8cc4ed7c9cb46b04428bd9b28811352652b7
tdmsp <tdmsp@iscte-iul.pt>
2021-05-05 21:03:57 +0100

    GUI Updated

--- src/main/java/es2sem2021/grupo2/codequalityassessor/gui/CodeSmellsPanel.java
+++ src/main/java/es2sem2021/grupo2/codequalityassessor/gui/CodeSmellsPanel.java
@@ -1,11 +1,14 @@
 package es2sem2021.grupo2.codequalityassessor.gui;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.awt.EventQueue;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -14,11 +17,16 @@ import javax.swing.JTable;
 import javax.swing.ScrollPaneConstants;
 import javax.swing.table.DefaultTableModel;
 
+import es2sem2021.grupo2.codequalityassessor.rules.CodeSmells;
 import es2sem2021.grupo2.codequalityassessor.rules.FinalResults;
 import es2sem2021.grupo2.codequalityassessor.rules.Rule;
-import es2sem2021.grupo2.codequalityassessor.rules.RuleResults;
 import es2sem2021.grupo2.codequalityassessor.rules.RulesSet;
 import es2sem2021.grupo2.codequalityassessor.xlsx.Method;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.ActionEvent;
 
 public class CodeSmellsPanel extends JPanel {
 
@@ -39,6 +47,8 @@ public class CodeSmellsPanel extends JPanel {
 				return false;
 			}
 		};
+		
+		
 		table = new JTable();
 		table.setModel(model);
 
@@ -48,24 +58,71 @@ public class CodeSmellsPanel extends JPanel {
 		table.setFillsViewportHeight(true);
 		scrollPane.setBounds(0, 0, 650, 483);
 		add(scrollPane);
+		
+		
+
+//		JScrollPane scrollPane = new JScrollPane(table);
+//		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
+//		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+//		scrollPane.setBounds(0, 0, 650, 483);
+//		add(scrollPane);
+
+//		JButton RefreshButton = new JButton("Refresh");
+//		RefreshButton.addMouseListener(new MouseAdapter() {
+//			@Override
+//			public void mouseClicked(MouseEvent e) {
+//				//updateCollumns
+//			}
+//		});
+
+//		RefreshButton.addActionListener(new ActionListener() {
+//			public void actionPerformed(ActionEvent arg0) {
+//			}
+//		});
+//		RefreshButton.setBounds(0, 0, 115, 56);
+//		add(RefreshButton);
+//		table = new JTable();
+//		table.setBounds(0, 73, 631, 392);
+//		add(table);
+//		table.setModel(model);
+//		table.setFillsViewportHeight(true);
 
 	}
 
-	public static void updateColunms(String[] colunm, List<Method> methods) {
+	public static void updateColunms( List<Method> methods) {
 		model.getDataVector().removeAllElements();
-		model.setColumnIdentifiers(colunm);
-		table.setModel(model);
 
-		ArrayList<RuleResults> ruleResults = FinalResults.getRulesResults(methods);
+		HashMap<String, HashMap<String, Boolean>> ruleResults = FinalResults.getRulesResults(methods);
 
-		for (int i = 0; i < methods.size(); i++) { 
-			Method method = methods.get(i);
-			model.addRow(new Object[] { method.m_class, method.m_method});
-			for (int j = 0; j < ruleResults.size(); j++) {
-				model.setValueAt(ruleResults.get(j).getMethodsresults().get(i).getResult(),i ,j+2);
+//		for (int i = 0; i < methods.size(); i++) { 
+//			Method method = methods.get(i);
+//			model.addRow(new Object[] { method.m_class, method.m_method});
+//			for (int j = 0; j < ruleResults.size(); j++) {
+//				model.setValueAt(ruleResults.get(j).getMethodsresults().get(i).getResult(),i ,j+2);
+//			}
+//			
+//		}
+		model.addColumn("Class");
+		model.addColumn("Method");
+
+		int j = 2;
+		for (Map.Entry<String, HashMap<String, Boolean>> entry : ruleResults.entrySet()) {
+			int i = 0;
+
+			if (!entry.getValue().isEmpty()) {
+				model.addColumn(entry.getKey());
+				for (Map.Entry<String, Boolean> entry1 : entry.getValue().entrySet()) {
+					if (j == 2) {
+						model.addRow(new Object[] { methods.get(i).m_class, methods.get(i).m_method });
+					}
+					model.setValueAt(entry1.getValue(), i, j);
+					i++;
+				}
+				j++;
+
 			}
-			
 		}
-	}
 
+		table.setModel(model);
+	}
 }
--- src/main/java/es2sem2021/grupo2/codequalityassessor/xlsx/CodeSmellsTableGenerator.java
+++ src/main/java/es2sem2021/grupo2/codequalityassessor/xlsx/CodeSmellsTableGenerator.java
@@ -17,7 +17,6 @@ import es2sem2021.grupo2.codequalityassessor.rules.Rule;
 import es2sem2021.grupo2.codequalityassessor.rules.RulesSet;
 
 public class CodeSmellsTableGenerator {
-	private String[] columnNames;
 
 	private File folder;
 
@@ -27,30 +26,17 @@ public class CodeSmellsTableGenerator {
 
 	public void main() throws IOException, InvalidFormatException {
 
-		ArrayList<String> ruleNamesArray = new ArrayList<String>();
-		ruleNamesArray.add("Class");
-		ruleNamesArray.add("Method");
-
-		HashMap<String, Rule> rules = RulesSet.getRules();
-
-		for (Map.Entry<String, Rule> entry : rules.entrySet()) {
-			String key = entry.getKey();
-			ruleNamesArray.add(key);
-		}
-		columnNames = ruleNamesArray.toArray(new String[0]);
 		List<Method> methods = new ArrayList<>();
 		parseFolders(folder, methods);
-		CodeSmellsPanel.updateColunms(columnNames, methods);
+		CodeSmellsPanel.updateColunms( methods);
 	}
 
-	public String[] getColumnNames() {
-		return columnNames;
-	}
-
+	
 	static void parseFolders(File folder, List<Method> methods) throws FileNotFoundException {
-		for (File f : folder.listFiles()) {
-			if (f.isDirectory())
-				parseFolders(f, methods);
+		for (File f: folder.listFiles()) {
+			//System.out.println(f.getName());
+			if(f.isDirectory())
+				parseFolders(f,methods);
 			else
 				methods.addAll(MetricsExtractor.extract(f));
 		}
