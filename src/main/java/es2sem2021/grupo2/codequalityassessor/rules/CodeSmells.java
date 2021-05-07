29209592069d58bb31f7d9483bd06028d22f93a6
dagds <dagds@iscte-iul.pt>
2021-05-07 17:14:43 +0100

    Codesmells are now persistant

--- src/main/java/es2sem2021/grupo2/codequalityassessor/rules/CodeSmells.java
+++ src/main/java/es2sem2021/grupo2/codequalityassessor/rules/CodeSmells.java
@@ -1,5 +1,11 @@
 package es2sem2021.grupo2.codequalityassessor.rules;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.HashMap;
 
 public class CodeSmells {
@@ -26,6 +32,7 @@ public class CodeSmells {
 			return false;
 
 		codesmells.put(codeSmell, rule);
+		saveToFile();
 		return true;
 	}
 
@@ -40,10 +47,9 @@ public class CodeSmells {
 			return false;
 
 		codesmells.remove(codeSmell);
+		saveToFile();
 		return true;
 	}
-
-
 
 	/**
 	 * If the adding process is successful returns true if not returns false
@@ -52,6 +58,48 @@ public class CodeSmells {
 	 */
 	public static HashMap<String, Rule> getCodeSmells() {
 		return codesmells;
+	}
+	
+	/**
+	 * Saves the rules into a file
+	 */
+	public static void saveToFile() {
+		try {
+			FileOutputStream f = new FileOutputStream(new File("codesmells"));
+			ObjectOutputStream o = new ObjectOutputStream(f);
+			
+			o.writeObject(codesmells);
+			
+			o.close();
+			f.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Loads the rules from a file
+	 */
+	@SuppressWarnings("unchecked")
+	public static void loadFromFile() {
+		try {
+			FileInputStream f = new FileInputStream(new File("codesmells"));
+			ObjectInputStream o = new ObjectInputStream(f);
+			
+			Object in = o.readObject();
+			
+			if (in instanceof HashMap) {
+				codesmells = (HashMap<String, Rule>) in;
+			}
+
+			o.close();
+			f.close();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
